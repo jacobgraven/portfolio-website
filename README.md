cf507c70302165bd9e4fc9d6e6c3696d8b1697c1
Jake <jacobmgraven@gmail.com>
2023-07-14 15:16:47 -0700

    major updates

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ To see the live website, visit [jgraven.com](https://jgraven.com).
 
 Icons were sourced and generated with a few third-party resources.[^1] Additional attributions are made in the source files.
 
-[^1]: Icons used in the footer were downloaded from [icons8.com](https://icons8.com) and favicons were created at [favicon.ico](https://favicon.io).
+[^1]: Icons used in the footer were downloaded from [iconify.design](https://iconify.design) and favicons were created at [favicon.ico](https://favicon.io).
--- js/animations.js
+++ js/animations.js
@@ -1,67 +1,9 @@
-// Typing text //
-document.addEventListener('DOMContentLoaded', () => {
-    const typeTime = 65;        // time (ms) delay between each character (includes deleting)
-    const delTime = 35;
-    const displayTime = 10000;  // time (ms) that completed message stays on screen
-    const restartTime = 1000;   // time (ms)
-    const permanentMessage = true; // animation will not loop (delete/retype) if this is true
-    const element = document.getElementById('welcome-text-type'); // doc element
-    const messages = ['Hello! This is placeholder text xO', '...example (2)'];
-    let msgIndex = 0;
-    let charIndex = 0;
-    function animateTyping(reversed) {
+// Return to top on refresh
+window.onbeforeunload = function () {
+    const elemnent1 = document.getElementById('');
+    window.scrollTo(0, 0);
+}
 
-        if (reversed) {
-            element.textContent = messages[msgIndex].slice(0, charIndex);
-            charIndex--;
-
-            if (charIndex < 0) {
-                msgIndex = (msgIndex + 1) % messages.length;
-                setTimeout(function () {
-                    animateTyping(false);
-                }, restartTime);
-            } else {
-                setTimeout(function () {
-                    animateTyping(true);
-                }, delTime);
-            }
-        } else {
-            element.textContent = messages[msgIndex].slice(0, charIndex + 1);
-            charIndex++;
-
-            if (charIndex === messages[msgIndex].length && permanentMessage) {     // message.length
-                // terminate animation loop (permanentMessage = true)
-                return;
-            } else if (charIndex === messages[msgIndex].length && !permanentMessage) {
-                setTimeout(function () {
-                    animateTyping(true);
-                }, displayTime);
-            } else {
-                setTimeout(function () {
-                    animateTyping(false);
-                }, typeTime);
-            }
-        }
-    }
-    // begin the animation loop
-    animateTyping(false);
-});
-
-
-// Fade-in text
-document.addEventListener('DOMContentLoaded', () => {
-    const element = document.getElementById('welcome-text-fade');
-    function animateFade(disabled = false) {
-        if (!disabled) {
-            element.classList.add('fade-in');
-        }
-    }
-
-    setTimeout(() => {
-        element.style.opacity = '1';
-        animateFade();
-    }, 4700);
-});
 
 // Navbar scroll
 document.addEventListener('DOMContentLoaded', () => {
@@ -85,31 +27,93 @@ document.addEventListener('DOMContentLoaded', () => {
             const targetSection = document.querySelector(targetSectionId);
 
             window.scrollTo({
-                top: targetSection.offsetTop - 7.5 * window.innerHeight/100,
+                top: targetSection.offsetTop - 7.5 * window.innerHeight / 100,
                 behavior: 'smooth'
             });
         });
     });
 
-    // Add event listener for scroll to update the active navbar link
     window.addEventListener('scroll', () => {
-        const currentScrollPos = window.pageYOffset;
+        const currentScrollPos = window.pageYOffset; // scrollY // pageYOffset
 
-        // Remove the active class from all navbar links
         navLinks.forEach(navLink => {
             navLink.classList.remove('active');
         });
 
-        // Add the active class to the corresponding navbar link based on the section in view
         navLinks.forEach(navLink => {
             const targetSectionId = navLink.getAttribute('href');
             const targetSection = document.querySelector(targetSectionId);
 
 
-            // todo
-            if (targetSection.offsetTop - 7.5 * window.innerHeight/100 <= currentScrollPos && targetSection.offsetTop + targetSection.offsetHeight - 7.5 * window.innerHeight/100 > currentScrollPos) {
-                navLink.classList.add('active');
-            }
+        
+            // if (targetSection.offsetTop - 7.5 * window.innerHeight / 100 <= currentScrollPos && targetSection.offsetTop + targetSection.offsetHeight - 7.5 * window.innerHeight / 100 > currentScrollPos) {
+            //     navLink.classList.add('active');
+            // }
         });
     });
 });
+
+
+// Typing text //
+// document.addEventListener('DOMContentLoaded', () => {
+//     const typeTime = 65;        // time (ms) delay between each character (includes deleting)
+//     const delTime = 35;
+//     const displayTime = 10000;  // time (ms) that completed message stays on screen
+//     const restartTime = 1000;   // time (ms)
+//     const permanentMessage = true; // animation will not loop (delete/retype) if this is true
+//     const element = document.getElementById('welcome-text-type'); // doc element
+//     const messages = ['Hello! This is placeholder text xO', '...example (2)'];
+//     let msgIndex = 0;
+//     let charIndex = 0;
+//     function animateTyping(reversed) {
+
+//         if (reversed) {
+//             element.textContent = messages[msgIndex].slice(0, charIndex);
+//             charIndex--;
+
+//             if (charIndex < 0) {
+//                 msgIndex = (msgIndex + 1) % messages.length;
+//                 setTimeout(function () {
+//                     animateTyping(false);
+//                 }, restartTime);
+//             } else {
+//                 setTimeout(function () {
+//                     animateTyping(true);
+//                 }, delTime);
+//             }
+//         } else {
+//             element.textContent = messages[msgIndex].slice(0, charIndex + 1);
+//             charIndex++;
+
+//             if (charIndex === messages[msgIndex].length && permanentMessage) {     // message.length
+//                 // terminate animation loop (permanentMessage = true)
+//                 return;
+//             } else if (charIndex === messages[msgIndex].length && !permanentMessage) {
+//                 setTimeout(function () {
+//                     animateTyping(true);
+//                 }, displayTime);
+//             } else {
+//                 setTimeout(function () {
+//                     animateTyping(false);
+//                 }, typeTime);
+//             }
+//         }
+//     }
+//     // begin the animation loop
+//     animateTyping(false);
+// });
+
+// Fade-in text
+// document.addEventListener('DOMContentLoaded', () => {
+//     const element = document.getElementById('welcome-text-fade');
+//     function animateFade(disabled = false) {
+//         if (!disabled) {
+//             element.classList.add('fade-in');
+//         }
+//     }
+
+//     setTimeout(() => {
+//         element.style.opacity = '1';
+//         animateFade();
+//     }, 4700);
+// });
